6a6a6b34205b6db15cb6852fe9973386b4629842
Zaz Brown <zazbrown@zazbrown.com>
2024-02-04 16:13:22 +0000

    fix: works!

--- PEMDAS.scm
+++ PEMDAS.scm
@@ -11,24 +11,35 @@
 ; XXX! Splitting by ` - ` first does not seem to make sense.
 ; Maybe EMDSA is more appropriate.
 
-(define (parse-by-precedence-backend precedence str)
+(define precedence '(#\+ #\- #\/ #\* #\^))
+(define execute    '(  +   -   /   * expt))
+
+(define (parse-by-precedence-backend precedence execute str)
   (if (null? precedence)
       (string->number str)
-      (map
-       (lambda (substr)
-         (parse-by-precedence-backend (cdr precedence) substr))
-       (string-split str (car precedence)))))
+      (let ((subtree
+             (map
+              (lambda (substr)
+                (parse-by-precedence-backend (cdr precedence) (cdr execute) substr))
+              (string-split str (car precedence)))))
+        (if (null? (cdr subtree))
+            (car subtree)
+            (cons (car execute) subtree)))))
 
-(define (parse-by-precedence precedence str)
+(define (parse-by-precedence precedence execute str)
   (parse-by-precedence-backend
-   (string->list precedence)
+   precedence
+   execute
    (string-filter (lambda (char) (not (eqv? char #\space))) str)))
 
 (define (main args)
   (test-begin "parse-by-precedence")
-  (display (parse-by-precedence "+-/*^" "1+2*3-4/5^6"))
+  (display (parse-by-precedence precedence execute "1+2*3-4/5^6"))
   (newline)
-  (test-eqv (parse-by-precedence "+-/*^" "1+2*3-4/5^6") '(+ 1 (- (* 2 3) (/ 4 (expt 5 6)))))
-  (test-eqv (parse-by-precedence "+-*/^" "7^8/9*2-4+3") '(+ (- (* (/ (expt 7 8) 9) 2) 4) 3))
-  (test-eqv (parse-by-precedence "+-/*^" "4-3+2*1/5^6") '(+ (- 4 3) (/ (* 2 1) (expt 5 6))))
+  (test-equal (parse-by-precedence precedence execute "1+2*3-4/5^6")
+           '(+ 1 (- (* 2 3) (/ 4 (expt 5 6)))))
+  (test-equal (parse-by-precedence '(#\+ #\- #\* #\/ #\^) '(+ - * / expt) "7^8/9*2-4+3")
+           '(+ (- (* (/ (expt 7 8) 9) 2) 4) 3))
+  (test-equal (parse-by-precedence precedence execute "4-3+2*1/5^6")
+           '(+ (- 4 3) (/ (* 2 1) (expt 5 6))))
   (test-end "parse-by-precedence"))
