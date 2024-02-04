74a77fcc77b68bc016faa1c82ebf97561a9bb668
Zaz Brown <zazbrown@zazbrown.com>
2024-02-04 04:03:28 +0000

    fix: it now compiles

--- PEMDAS.scm
+++ PEMDAS.scm
@@ -1,24 +1,30 @@
+#!/usr/bin/guile \
+-e main -s
+!#
 ; A simple parser for PEMDAS arithmetic
+
+; Use SRFI-64 testing framework.
+(use-modules (srfi srfi-64))
 
 ; First, implement an EMDAS parser.
 ; We will first split by ` - `, then ` + `, ` / `, and finally ` * `.
 ; XXX! Splitting by ` - ` first does not seem to make sense.
 ; Maybe EMDSA is more appropriate.
 
-(defun test-parse-by-precedence ()
-  (assert (equal (parse-by-precedence "+-/*^" "1+2*3-4/5^6") '(+ 1 (- (* 2 3) (/ 4 (^ 5 6))))))
-  (assert (equal (parse-by-precedence "+-*/^" "7^8/9*2-4+3") '(+ (- (* (/ (^ 7 8) 9) 2) 4) 3)))
-  (assert (equal (parse-by-precedence "+-/*^" "4-3+2*1/5^6") '(+ (- 4 3) (/ (* 2 1) (^ 5 6))))))
-
-(defun parse-by-precedence-backend (precedence str)
+(define (parse-by-precedence-backend precedence str)
   (for-each
    (lambda (substr)
      (parse-by-precedence-backend (cdr precedence) substr))
    (split-string str (car precedence) t)))
 
-(defun parse-by-precedence (precedence str)
+(define (parse-by-precedence precedence str)
   (parse-by-precedence-backend
    (string->list precedence)
    (replace-regexp-in-string " " "" str)))
 
-(test-parse-by-precedence)
+(define (main args)
+  (test-begin "parse-by-precedence")
+  (test-assert (equal (parse-by-precedence "+-/*^" "1+2*3-4/5^6") '(+ 1 (- (* 2 3) (/ 4 (expt 5 6))))))
+  (test-assert (equal (parse-by-precedence "+-*/^" "7^8/9*2-4+3") '(+ (- (* (/ (expt 7 8) 9) 2) 4) 3)))
+  (test-assert (equal (parse-by-precedence "+-/*^" "4-3+2*1/5^6") '(+ (- 4 3) (/ (* 2 1) (expt 5 6)))))
+  (test-end "parse-by-precedence"))
