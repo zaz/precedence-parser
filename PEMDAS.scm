b58d990823c8340d4c4fc78db0fb3472a9bcb8e7
Zaz Brown <zazbrown@zazbrown.com>
2024-02-04 15:50:47 +0000

    WIP: fix: move towards getting it working
    Compiles, but does not work correctly.

--- PEMDAS.scm
+++ PEMDAS.scm
@@ -12,19 +12,23 @@
 ; Maybe EMDSA is more appropriate.
 
 (define (parse-by-precedence-backend precedence str)
-  (for-each
-   (lambda (substr)
-     (parse-by-precedence-backend (cdr precedence) substr))
-   (split-string str (car precedence) t)))
+  (if (null? precedence)
+      (string->number str)
+      (map
+       (lambda (substr)
+         (parse-by-precedence-backend (cdr precedence) substr))
+       (string-split str (car precedence)))))
 
 (define (parse-by-precedence precedence str)
   (parse-by-precedence-backend
    (string->list precedence)
-   (replace-regexp-in-string " " "" str)))
+   (string-filter (lambda (char) (not (eqv? char #\space))) str)))
 
 (define (main args)
   (test-begin "parse-by-precedence")
-  (test-assert (equal (parse-by-precedence "+-/*^" "1+2*3-4/5^6") '(+ 1 (- (* 2 3) (/ 4 (expt 5 6))))))
-  (test-assert (equal (parse-by-precedence "+-*/^" "7^8/9*2-4+3") '(+ (- (* (/ (expt 7 8) 9) 2) 4) 3)))
-  (test-assert (equal (parse-by-precedence "+-/*^" "4-3+2*1/5^6") '(+ (- 4 3) (/ (* 2 1) (expt 5 6)))))
+  (display (parse-by-precedence "+-/*^" "1+2*3-4/5^6"))
+  (newline)
+  (test-eqv (parse-by-precedence "+-/*^" "1+2*3-4/5^6") '(+ 1 (- (* 2 3) (/ 4 (expt 5 6)))))
+  (test-eqv (parse-by-precedence "+-*/^" "7^8/9*2-4+3") '(+ (- (* (/ (expt 7 8) 9) 2) 4) 3))
+  (test-eqv (parse-by-precedence "+-/*^" "4-3+2*1/5^6") '(+ (- 4 3) (/ (* 2 1) (expt 5 6))))
   (test-end "parse-by-precedence"))
